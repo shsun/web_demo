1edc12173773c1d3c2db19a3dfe342d8f4e4c620
sunshanghai <sunshanghai@baidu.com>
2016-12-13 21:21:35 +0800

    init

--- src/main/java/com/shsun/addata/vo/base/AbstractTableSelectionStrategy.java
+++ src/main/java/com/shsun/addata/vo/base/AbstractTableSelectionStrategy.java
@@ -7,32 +7,32 @@ package com.shsun.addata.vo.base;
  */
 public abstract class AbstractTableSelectionStrategy implements ITableSelectionStrategy {
 
-	/**
-	 * 
-	 */
-	protected IHttpServletRequestParameter httpServletRequestParameter;
-	/**
-	 * 
-	 */
-	protected ISqlParameterSource sqlParameterSource;
+    /**
+     * 
+     */
+    protected IHttpServletRequestParameter httpServletRequestParameter;
+    /**
+     * 
+     */
+    protected ISqlParameterSource sqlParameterSource;
 
-	/**
-	 * 
-	 * @param httpServletRequestParameter
-	 * @param sqlParameterSource
-	 */
-	public AbstractTableSelectionStrategy(IHttpServletRequestParameter httpServletRequestParameter, ISqlParameterSource sqlParameterSource) {
-		this.httpServletRequestParameter = httpServletRequestParameter;
-		this.sqlParameterSource = sqlParameterSource;
-	}
+    /**
+     * 
+     * @param httpServletRequestParameter
+     * @param sqlParameterSource
+     */
+    public AbstractTableSelectionStrategy(IHttpServletRequestParameter httpServletRequestParameter, ISqlParameterSource sqlParameterSource) {
+        this.httpServletRequestParameter = httpServletRequestParameter;
+        this.sqlParameterSource = sqlParameterSource;
+    }
 
-	// ----------------------------------------------------------------------------------------------------------------------------------
-	/**
-	 * select the most suitable table for querying.
-	 */
-	public void select() {
-		this.doSelect();
-	}
+    // ----------------------------------------------------------------------------------------------------------------------------------
+    /**
+     * select the most suitable table for querying.
+     */
+    public void select() {
+        this.doSelect();
+    }
 
-	protected abstract void doSelect();
+    protected abstract void doSelect();
 }
